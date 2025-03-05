352407354e1b2dd12a04f5781af32b70247f919a
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-05 23:40:17 +0530

    refactor: place prisma client shutdown handlers

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const dotenv = require("dotenv");
 const express = require("express");
 
+const prismaClient = require("./prisma/client");
+
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Express server listening on PORT: ${PORT}...`)
+});
+
+// Graceful shutdown handling. Modularize these later into its own files
+process.on('SIGINT', async () => {
+    console.log('SIGINT received - closing server');
+    await prisma.$disconnect();
+    server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', async () => {
+    console.log('SIGTERM received - closing server');
+    await prisma.$disconnect();
+    server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    });
 });
