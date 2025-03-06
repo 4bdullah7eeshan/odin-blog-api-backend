2c0ae5cb05d9da721a682a4673e90292a060a0a4
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-07 01:00:48 +0530

    wip: associate auth routes and controllers

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,13 +1,16 @@
 const { Router } = express();
-// Import Controllers here
-// Perhaps, other stuff too
+const {
+    createANewUser,
+    signInAUser,
+    signOutAUser
+} = require("../controllers/userControllers");
 
 const userRouter = Router();
 
 // Setup routes here
-userRouter.post("/sign-up"); // Create a new user
-userRouter.post("/sign-in");
-userRouter.post("/sign-out");
+userRouter.post("/sign-up", createANewUser); // Create a new user
+userRouter.post("/sign-in", signInAUser);
+userRouter.post("/sign-out", signOutAUser);
 
 // Let me tackle other routes later. Need to sort this authentication stuff
 
