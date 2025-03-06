0c7c93cf29b65cb0fe1d9b3159c06b13accb2a26
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-06 05:45:10 +0530

    feat: use routes in app.js

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const express = require("express");
 const { PORT } = require("./config/index");
 const prismaClient = require("./prisma/client");
 
+const userRouter = require("./routes/userRouter");
+const postRouter = require("./routes/postRouter");
+const commentRouter = require("./routes/commentRouter");
+
 const app = express();
+
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
 
 app.listen(PORT, () => {
     console.log(`Express server listening on PORT: ${PORT}...`)
