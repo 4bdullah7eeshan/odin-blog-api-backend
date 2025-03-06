8ba22ae7463264460359448744e09907cb86c7cc
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-07 00:56:23 +0530

    wip: setup auth routes for user

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -5,7 +5,11 @@ const { Router } = express();
 const userRouter = Router();
 
 // Setup routes here
-userRouter.post("/"); // Create a new user
+userRouter.post("/sign-up"); // Create a new user
+userRouter.post("/sign-in");
+userRouter.post("/sign-out");
+
+// Let me tackle other routes later. Need to sort this authentication stuff
 
 
 module.exports = userRouter;
