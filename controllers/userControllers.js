ba068d39627b4b63e8209b78ff96071488d5ded2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-07 00:58:59 +0530

    wip: declare auth controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,6 +3,18 @@ const prisma = require("../prisma/client");
 
 // Think about these later
 
+const createANewUser = asyncHandler(async (req, res) => {
+    // goes with sign up route
+});
+
+const signInAUser = asyncHandler(async (req, res) => {
+
+});
+
+const signOutAUser = asyncHandler(async (req, res) => {
+
+});
+
 // Just pacing them here because this is where they must belong. 
 // Not sure, if I should use them now. If wanna use, setup a subroute/subresource under "users"
 const getAllPostsByAUser = asyncHandler(async (req, res) => {
@@ -15,5 +27,8 @@ const deleteAllPostsOfAUser = asyncHandler(async (req, res) => {
 
 
 module.exports = {
+    createANewUser,
+    signInAUser,
+    signOutAUser,
     
 }
