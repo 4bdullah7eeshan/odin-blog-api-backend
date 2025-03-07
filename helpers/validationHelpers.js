f46fbd79285bd679e4653e9f9b0753a3c998c087
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2025-03-08 04:08:04 +0530

    feat: create and setup validation helpers

---
+++ helpers/validationHelpers.js
@@ -0,0 +1,31 @@
+const { body } = require("express-validator");
+const recordExists = require("./prismaHelpers");
+
+const notEmpty = (fieldName, customMessage) =>
+    body(fieldName)
+        .trim()
+        .notEmpty()
+        .withMessage(customMessage || `${fieldName} cannot be empty`);
+
+
+const hasLength = (fieldName, min, max, customMessage) =>
+    body(fieldName)
+        .isLength({ min, max })
+        .withMessage(customMessage || `${fieldName} must be between ${min} and ${max} characters`);
+
+
+const isUnique = (fieldName, model, dbField, customMessage) =>
+    body(fieldName)
+        .custom(async (value) => {
+            const exists = await recordExists(model, dbField, value);
+            if (exists) {
+                return Promise.reject(customMessage || `${fieldName} is already in use`);
+            }
+            return true;
+        });
+                
+module.exports = {
+    notEmpty,
+    hasLength,
+    isUnique,
+}
